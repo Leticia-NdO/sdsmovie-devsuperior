6466d08d344edd28c16b462bf54c9249b97bd287
Leticia-NdO <3972656@alunouninter.com>
2022-05-10 14:19:51 -0300

    MovieCard

---
+++ frontend/src/components/movieScore/index.tsx
@@ -0,0 +1,17 @@
+import MovieStars from "components/movieStars"
+import './styles.css'
+function MovieScore() {
+
+    const score = 3.59;
+    const count = 13;
+
+    return (
+        <div className="dsmovie-score-container">
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>   {/* se a nota for maior que zero, vai mostrar o score arredondado com uma casa decimal. Senão, mostra só m tracinho */}
+            <MovieStars />  {/* um component que contém outro component */}
+            <p className="dsmovie-score-count">{count} avaliações</p>
+        </div>
+    )
+}
+
+export default MovieScore
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,29 @@
 // cria-se duas pastas, components e pages, pra diferenciar os componentes que são pedaços do site dos que são rotas.
+import MovieCard from "components/movieCard";
+import MovieScore from "components/movieScore";
 import Pagination from "components/pagination";
 
-function Listing(){
+function Listing() {
     return (
-        <Pagination/>
+        <>  {/* a função que é um componente react só pode exportar um componente, mas é ó colocar entre uma div ou um fragmente que ele aceita */}
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela. O mb-3 (margin bottom) coloca uma pequena margem abaixo do objeto*/}
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela */}
+                        <MovieCard />
+                    </div>                    
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela */}
+                        <MovieCard />
+                    </div>                    
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela */}
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
