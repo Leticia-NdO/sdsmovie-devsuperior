57b412b5d87f2c36eb03be6a9bd2a374779ce30e
Leticia-NdO <3972656@alunouninter.com>
2022-05-15 13:09:30 -0300

    Show score

--- frontend/src/components/movieCard/index.tsx
+++ frontend/src/components/movieCard/index.tsx
@@ -13,7 +13,7 @@ function MovieCard({ movie }: Props) {  // Props podem ser entendidas como argum
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />    {/* ele já está estilizado porque estámos usando classes do Form, e como o CSS é global, uma vez importado ele está disponível em qualquer lugar. */}
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score}/>
 
                 <Link to={`form/${movie.id}`} >   {/* uma propriedade do react router dom que permite fácil linkagem dos elementos */}
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
--- frontend/src/components/movieScore/index.tsx
+++ frontend/src/components/movieScore/index.tsx
@@ -1,14 +1,19 @@
+import axios from "axios"
 import MovieStars from "components/movieStars"
+import { useEffect } from "react"
 import './styles.css'
-function MovieScore() {
 
-    const score = 3.59;
-    const count = 13;
+type Props = {
+    score: number,
+    count: number
+}
+
+function MovieScore({score, count}: Props) {
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>   {/* se a nota for maior que zero, vai mostrar o score arredondado com uma casa decimal. Senão, mostra só m tracinho */}
-            <MovieStars />  {/* um component que contém outro component */}
+            <MovieStars score={score}/>  {/* um component que contém outro component */}
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     )
