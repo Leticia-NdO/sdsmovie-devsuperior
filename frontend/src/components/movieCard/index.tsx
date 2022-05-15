d7fc9e849e7b0c23f717fb16a86ac64f545b935c
Leticia-NdO <3972656@alunouninter.com>
2022-05-15 11:31:22 -0300

    Props

--- frontend/src/components/movieCard/index.tsx
+++ frontend/src/components/movieCard/index.tsx
@@ -1,15 +1,12 @@
 import MovieScore from "components/movieScore"
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
+type Props = {
+    movie: Movie;
+}
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+function MovieCard({ movie }: Props) {  // Props podem ser entendidas como argumentos do componente React. Aqui, e estabeleço que o MovieCard precisa de um parâmetro do tipo Movie.
 
     return (
         <div>
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -10,36 +10,46 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0)   // useState serve para armazenar estados dentro do componente. O primeiro item da lista é a variável e o segundo é a função que serve para manipular essa variável. O primeira parâmetro de useState é o valor da variável
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true,
+    })
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`).then(response => {
             const data = response.data as MoviePage;
-            console.log(data)
-            setPageNumber(data.number);
+            setPage(data);
         })
-    }, [])  // o useEffect xecuta algo na instanciação ou destruição do componente e observa estado. Ele recebe dois parâmetros, uma função e uma lista de objetos que vai observar. Sempre que um desses objetos sofrer uma alteração, ele vai executar a função. Se a lista estiver vazia ele só executa a função quando o componente for carregado.
-
-    // FORMA ERRADA
+    }, [pageNumber])  // o useEffect xecuta algo na instanciação ou destruição do componente e observa estado. Ele recebe dois parâmetros, uma função e uma lista de objetos que vai observar. Sempre que um desses objetos sofrer uma alteração, ele vai executar a função. Se a lista estiver vazia ele só executa a função quando o componente for carregado. Nesse caso, quando mudar o pageNumber ele vai refazer essa função e3 atualizar a página.
 
 
-
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
     return (
         <>  {/* a função que é um componente react só pode exportar um componente, mas é ó colocar entre uma div ou um fragmente que ele aceita */}
-            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela. O mb-3 (margin bottom) coloca uma pequena margem abaixo do objeto*/}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela */}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela */}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela */}
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">  {/* o grid system do bootstrap divide a tela em 12 colunas padrão, ao dizer sm (small) 6, eu digo que essa coluna deve ocupar 6 desses espaços, ou seja, metade da tela. O mb-3 (margin bottom) coloca uma pequena margem abaixo do objeto*/}
+                                <MovieCard movie={movie} />    {/* aqui eu digo que o argumento do MovieCard, o parâmetro movie, é o objeto movie ali de cima */}
+                            </div>
+                        )
+                    )}
+                    <div />
+
                 </div>
             </div>
         </>
